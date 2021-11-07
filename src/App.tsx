8984f29bf4f61631fd35530daca653f199dfc7fd
LindseyRutledge <67494680+LindseyRutledge@users.noreply.github.com>
2021-11-07 01:09:37 -0700

    adding infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Masonry from 'react-masonry-component';
+import './App.css';
 
 function App() {
   const [weddingPhotos, setWeddingPhotos] = useState<any[]>([]);
+  const [visibleCount, setVisibleCount] = useState<number>(10);
 
   const importAll = (r: any) => {
     return r.keys().map(r);
   };
 
+  const getImageName = (filePath: string): string => {
+    const filePathSplit: string[] = filePath.split('/');
+    const fileNameWithExtension = filePathSplit[filePathSplit.length - 1];
+
+    const fileNameSplit: string[] = fileNameWithExtension.split('.');
+    const fileName = fileNameSplit[0];
+
+    return fileName;
+  };
+
   useEffect(() => {
-    setWeddingPhotos(importAll(require.context('./assets/wedding-photos', false, /\.(png|jpe?g|svg)$/)));
+    const allPhotos = importAll(require.context('./assets/wedding-photos', false, /\.(png|jpe?g|svg)$/));
+    allPhotos.sort((a: any, b: any) => {
+      const photoNameA = getImageName(a.default);
+      const photoNameB = getImageName(b.default);
+
+      return photoNameA.localeCompare(photoNameB, undefined, { numeric: true, sensitivity: 'base' });
+    });
+    setWeddingPhotos(allPhotos);
   }, []);
 
-  console.log(weddingPhotos);
+  const hasMore = visibleCount < weddingPhotos.length;
 
   return (
     <div className='App'>
@@ -21,20 +40,33 @@ function App() {
         className='wedding-photos'
         options={{
           columnWidth: '.wedding-photo',
-          itemSelector: '.wedding-photo',
+          itemSelector: '.wedding-photo, .wedding-photo-bookend',
           gutter: 10,
           fitWidth: true,
-          percentPosition: true,
         }}
-        // @ts-ignore
-        imagesLoadedOptions={{ background: '.wedding-photo' }}
       >
-        {weddingPhotos.map((image, index) => {
-          return (
-            <img className='wedding-photo' key={index} src={image?.default} />
-          );
-        })}
+        <InfiniteScroll
+          dataLength={visibleCount}
+          next={() => setTimeout(() => {
+            setVisibleCount(visibleCount + 5);
+          }, 1000)}
+          hasMore={hasMore}
+          loader={<div>Loading...</div>}
+        >
+          {weddingPhotos.slice(0, visibleCount).map((image, index) => {
+            return (
+              <a href={image?.default} title="View Image" target="_blank">
+                <img
+                  className={index === 0 || index === weddingPhotos.length - 1 ? 'wedding-photo-bookend' : 'wedding-photo'}
+                  key={index}
+                  src={image?.default}
+                />
+              </a>
+            );
+          })}
+        </InfiniteScroll>
       </Masonry>
+      <div style={{ color: 'white', display: hasMore ? '' : 'none' }}>Loading....</div>
     </div>
   );
 }
